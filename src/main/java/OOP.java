b25dae710fc30139864138a4de1f147114da04ea
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-06-06 15:08:30 +0200

    Add Interface

--- src/main/java/OOP.java
+++ src/main/java/OOP.java
@@ -1,6 +1,19 @@
-abstract class Vehicle {
 
-    abstract void accelerate();
+interface Accelerateable {
+
+    void accelerate();
+}
+
+interface Hoonkable {
+    void hoonk();
+}
+
+interface Driveable{
+    void accelerate();
+    void breaking();
+}
+
+abstract class Vehicle implements Accelerateable, Hoonkable{
 
     void breaking() {
         System.out.println("Hitting the breaks");
@@ -10,8 +23,13 @@ abstract class Vehicle {
 class Bike extends Vehicle {
 
     @Override
-    void accelerate() {
+    public void accelerate() {
         System.out.println("Pedalling");
+    }
+
+    @Override
+    public void hoonk() {
+        System.out.println("klingel");
     }
 }
 
@@ -19,14 +37,29 @@ class Car extends Vehicle {
     boolean hasFourWheels = true;
 
     @Override
-    void accelerate() {
+    public void accelerate() {
         System.out.println("Hitting the gas");
+    }
+
+    @Override
+    public void hoonk() {
+        System.out.println("hupe");
     }
 }
 
-class BMW extends Car {
+class BMW extends Car implements Driveable {
 
     String logo = "blue and white";
+
+    @Override
+    public void hoonk() {
+        System.out.println("laute hupe");
+    }
+
+    @Override
+    public void breaking() {
+        System.out.println("break");
+    }
 }
 
 public class OOP {
@@ -55,5 +88,9 @@ public class OOP {
 
         Integer number = 1;
         Double number2 = 1.1;
+
+        BMW bmw = new BMW();
+        bmw.accelerate();
+        bmw.hoonk();
     }
 }
